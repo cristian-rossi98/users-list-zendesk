7a2c557583acc2a571488a3c12849708ca1dc6eb
Cristian Rossi <cristian.rossicosta@gmail.com>
2023-12-14 18:19:05 -0300

    remove comments

--- assets/controller/main.controller.js
+++ assets/controller/main.controller.js
@@ -2,22 +2,4 @@ angularjs.controller("angularjsController", function ($scope, zendeskService) {
   zendeskService.listUsers().then(function (users) {
     $scope.arrayUsers = users.users;
   });
-  // zendeskService.listTickets().then(function (tickets) {
-  //   console.log("TICKETS: ", tickets.tickets);
-  // });
-  // let ticket = {
-  //   ticket: {
-  //     comment: {
-  //       body: "Ticket criado via app",
-  //     },
-  //     priority: "urgent",
-  //     subject: "Minha primeira requisição via app",
-  //   },
-  // };
-  // zendeskService.createTicket(ticket).then(function (data) {
-  //   console.log("TICKET CRIADO: ", data);
-  // });
-  // client.metadata().then(function (parameters) {
-  //   console.log("Manifest: ", parameters);
-  // });
 });
